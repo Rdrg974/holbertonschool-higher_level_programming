3f9ee85ac52f7c4bcacae9ebb5c8de8b8b2010c1
Rdrg974 <hassany.rodrigue@gmail.com>
2024-01-22 06:47:44 -0800

    Use the word add_0 once in my code

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import add_0
+    from add_0 import add
     a = 1
     b = 2
-    result = add_0.add(a, b)
+    result = add(a, b)
     print("{} + {} = {}".format(a, b, result))
