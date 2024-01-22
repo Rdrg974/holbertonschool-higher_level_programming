b8aa5a065c9b3d8d179507abb7dd7cb842a39ca3
Rdrg974 <hassany.rodrigue@gmail.com>
2024-01-22 00:51:52 -0800

    Python Script

---
+++ python-hello_world/5-print_string.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+str = "Holberton School"
+print(f"{str}{str}{str}")
+print(str[0:9])
---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str[39:67] + str[-22:-17] + str[0:6]
+print(str)
