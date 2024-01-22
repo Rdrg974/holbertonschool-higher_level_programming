41b09b2ba8aba1a8d2aa32b613314dba9980749e
Rdrg974 <hassany.rodrigue@gmail.com>
2024-01-22 01:10:00 -0800

    Update the script

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 word = "Holberton"
 word_first_3 = word[0:3]
-word_last_2 = word[7:]
+word_last_2 = word[-2:]
 middle_word = word[1:8]
 print(f"First 3 letters: {word_first_3}")
 print(f"Last 2 letters: {word_last_2}")
