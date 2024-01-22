a7fbf94f81d40145a0763baba5387f40606e178e
Rdrg974 <hassany.rodrigue@gmail.com>
2024-01-22 06:45:54 -0800

    To determine whether the file is run as a script or imported as a module in another script

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-import add_0
-a = 1
-b = 2
-print("{} + {} = {}".format(a, b, add_0.add(1, 2)))
+if __name__ == "__main__":
+    import add_0
+    a = 1
+    b = 2
+    result = add_0.add(a, b)
+    print("{} + {} = {}".format(a, b, result))
