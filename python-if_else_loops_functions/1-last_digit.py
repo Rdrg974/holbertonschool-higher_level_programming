9ff2c48d10a98ec6c4b1b22333edd523c7587d33
Rdrg974 <hassany.rodrigue@gmail.com>
2024-01-22 02:39:00 -0800

    Shorten the variable last_digit

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,16 +2,15 @@
 import random
 number = random.randint(-10000, 10000)
 number_copie = number
-last_digit = 0
+l_digit = 0
 if number < 0:
     number_copie *= -1
-    last_digit = -1 * (number_copie % 10)
+    l_digit = -1 * (number_copie % 10)
 else:
-    last_digit = number % 10
-if last_digit > 5:
-    print(f"Last digit of {number} is {last_digit} and is greater than 5")
-elif last_digit == 0:
-    print(f"Last digit of {number} is {last_digit} and is 0")
+    l_digit = number % 10
+if l_digit > 5:
+    print(f"Last digit of {number} is {l_digit} and is greater than 5")
+elif l_digit == 0:
+    print(f"Last digit of {number} is {l_digit} and is 0")
 else:
-    print(f"Last digit of {number} is {last_digit}\
-            and is less than 6 and not 0")
+    print(f"Last digit of {number} is {l_digit} and is less than 6 and not 0")
