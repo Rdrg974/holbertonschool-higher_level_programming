5b2fe906e65d45efd92036e214a0529a28eec117
Rdrg974 <hassany.rodrigue@gmail.com>
2024-01-23 01:17:11 -0800

    Fix pycodestyle and the number of arguments

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys, calculator_1 as cal
-    if len(sys.argv) < 3:
+    import sys
+    import calculator_1 as cal
+    if len(sys.argv) != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
     else:
