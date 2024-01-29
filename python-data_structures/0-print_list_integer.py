0e067d5fd78f3d84bd82744b16cab09956c3ab3f
Rdrg974 <hassany.rodrigue@gmail.com>
2024-01-29 00:34:06 -0800

    Change the for by a while

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for i in range(len(my_list)):
+    i = 0
+    while (i < len(my_list)):
         print("{}".format(my_list[i]))
+        i = i + 1
