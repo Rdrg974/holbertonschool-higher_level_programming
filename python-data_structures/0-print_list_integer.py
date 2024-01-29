f85932cd5162b7b9c49cc02bb0f0fc41f974f9da
Rdrg974 <hassany.rodrigue@gmail.com>
2024-01-29 01:15:17 -0800

    Update

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    if my_list is None:
-        return None
-    for i in my_list:
-        print("{}".format(i))
+    for i in range(len(my_list)):
+        print("{:d}".format(my_list[i]))
