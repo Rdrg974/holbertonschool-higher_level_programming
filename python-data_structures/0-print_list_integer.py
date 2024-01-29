90cf50e954472d097f44c37b585f1d359dfbe50b
Rdrg974 <hassany.rodrigue@gmail.com>
2024-01-29 00:51:13 -0800

    Update

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    i = 0
-    while (i < len(my_list)):
-        print("{}".format(my_list[i]))
-        i = i + 1
+    for i in my_list:
+        print("{}".format(i))
