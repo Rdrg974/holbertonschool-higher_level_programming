d0e99f23ea7aea86a03aa52115794f23b3b6e139
Abdou Rodrigue Hassany Mohamed <hassany.rodrigue@gmail.com>
2024-01-29 14:21:02 +0000

    Fix idx

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx >= len(my_list):
         return my_list
     else:
         del my_list[idx]
