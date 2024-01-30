9b577e6ff714aa3a319ec4241ff3ce1f107db0d0
Abdou Rodrigue Hassany Mohamed <hassany.rodrigue@gmail.com>
2024-01-30 22:03:14 +0000

    Fix line too long and over-indented

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,12 +2,12 @@
 def roman_to_int(roman_string):
     if not roman_string or not isinstance(roman_string, str):
         return 0
-    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
-                  'C': 100, 'D': 500, 'M': 1000}
+    r_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+              'C': 100, 'D': 500, 'M': 1000}
     num = 0
     for i in range(len(roman_string)):
-        if i > 0 and roman_dict[roman_string[i]] > roman_dict[roman_string[i - 1]]:
-            num += roman_dict[roman_string[i]] - 2 * roman_dict[roman_string[i - 1]]
+        if i > 0 and r_dict[roman_string[i]] > r_dict[roman_string[i - 1]]:
+            num += r_dict[roman_string[i]] - 2 * r_dict[roman_string[i - 1]]
         else:
-            num += roman_dict[roman_string[i]]
+            num += r_dict[roman_string[i]]
     return num
