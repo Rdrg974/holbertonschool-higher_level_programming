c69744e03584b7ed56e6a2e9d9335fd0f8c4a283
Abdou Rodrigue Hassany Mohamed <hassany.rodrigue@gmail.com>
2024-02-05 21:44:13 +0000

    Fix docstring

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -7,14 +7,12 @@ TypeError
     If the size provided is not an integer.
 ValueError
     If the size provided is less than zero.
-
 Returns
 -------
 int
     The area of the square.
 tuple
     The position of the square's top-left corner.
-
 Attributes
 ----------
 size : int
@@ -34,14 +32,17 @@ class Square:
     position : tuple of int
         The position of the square's top-left corner.
     """
+
     __size = None
 
     @property
     def size(self):
+        """int: The size of the square's sides."""
         return self.__size
 
     @size.setter
     def size(self, value):
+        """Set the size of the square's sides."""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
@@ -53,10 +54,12 @@ class Square:
 
     @property
     def position(self):
+        """The position of the square's top-left corner."""
         return self.__position
 
     @position.setter
     def position(self, value):
+        """Set the position of the square's top-left corner."""
         if not isinstance(value, tuple) or len(value) != 2 or \
             not all(isinstance(v, int) for v in value) or \
                 not all(i >= 0 for i in value):
@@ -65,13 +68,16 @@ class Square:
             self.__position = value
 
     def __init__(self, __size=0, __position=(0, 0)):
+        """Initialize the square."""
         self.__size = __size
         self.__position = __position
 
     def area(self):
+        """Return the size of the square."""
         return self.__size * self.__size
 
     def my_print(self):
+        """Print the square with the given size and position."""
         if self.__size == 0:
             print()
         else:
