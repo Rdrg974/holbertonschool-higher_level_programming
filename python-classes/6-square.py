951010e4e5e799d3d0bdaf1aba6b5e8da4a61bfd
Abdou Rodrigue Hassany Mohamed <hassany.rodrigue@gmail.com>
2024-02-05 19:09:54 +0000

    Fix line too long

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -44,7 +44,8 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if not isinstance(value, tuple) or not all(v >= 0 for v in value):
+        if not isinstance(value, tuple) or not all(v >= 0 for v in value) or \
+                len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
@@ -60,12 +61,7 @@ class Square:
         if self.__size == 0:
             print()
         else:
-            if self.__position[1] > 0:
-                for i in range(self.__position[1]):
-                    print()
-            for j in range(self.__size):
-                for k in range(self.__position[0]):
-                    print(" ", end='')
-                for n in range(self.__size):
-                    print("#", end='')
+            for i in range(self.__position[1]):
                 print()
+            for j in range(self.__size):
+                print(" " * self.__position[0] + "#" * self.__size)
