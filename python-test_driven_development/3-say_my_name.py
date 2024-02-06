a9021816b3936c2cbfb9d49a62b041eeae3d19f1
Abdou Rodrigue Hassany Mohamed <hassany.rodrigue@gmail.com>
2024-02-06 18:23:33 +0000

    Function that prints My name is <first name> <last name>

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def say_my_name(first_name, last_name=""):
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
