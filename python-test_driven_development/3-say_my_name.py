5863ffd9e6edf4815bd19e43cf3fc61dff94aa55
Abdou Rodrigue Hassany Mohamed <hassany.rodrigue@gmail.com>
2024-02-06 18:25:45 +0000

    Fix pycodestyle

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""Function that prints My name is <first name> <last name>."""
+
+
 def say_my_name(first_name, last_name=""):
+    """Print My name is <first name> <last name>."""
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
