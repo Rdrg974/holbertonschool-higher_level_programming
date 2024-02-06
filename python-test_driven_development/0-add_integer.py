94acc13ddd7e7330e7486e78b0c28e798bc2ad86
Abdou Rodrigue Hassany Mohamed <hassany.rodrigue@gmail.com>
2024-02-06 15:00:48 +0000

    Fix pycodestyle

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -3,9 +3,20 @@
 
 
 def add_integer(a, b=98):
-    
+    """Add 2 integers.
+
+    Args:
+        a (int, float): a integer
+        b (int, float): an integer. Defaults to 98.
+
+    Raises:
+        TypeError: a and b must be an integer
+
+    Returns:
+        _type_: the sum of a and b
+    """
     if not (isinstance(a, (int, float)) or isinstance(a, (int, float))):
         raise TypeError("a must be an integer")
     elif not (isinstance(b, (int, float)) or isinstance(b, (int, float))):
         raise TypeError("b must be an integer")
-    return int(a)+ int(b)
+    return int(a) + int(b)
