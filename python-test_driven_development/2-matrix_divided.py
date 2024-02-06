f184f295352df4d51665a93a4856380dfd1b7861
Abdou Rodrigue Hassany Mohamed <hassany.rodrigue@gmail.com>
2024-02-06 17:47:03 +0000

    Fix pycodestyle

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,13 +1,31 @@
 #!/usr/bin/python3
+"""Function that divides all elements of a matrix."""
 
 
 def matrix_divided(matrix, div):
+    """Divide all elements of a matrix.
+
+    Args:
+        matrix (list): matrix to divide
+        div (int, float): number to divide by
+
+    Raises:
+        TypeError: div must be a number
+        ZeroDivisionError: division by zero
+        TypeError: matrix must be a matrix (list of lists) of integers/floats
+        TypeError: Each row of the matrix must have the same size
+
+    Returns:
+        _type_: a new matrix with the result of the division
+    """
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if not isinstance(matrix, list) or \
+            not all(isinstance(row, list) for row in matrix):
+        raise TypeError("matrix must be a matrix \
+            (list of lists) of integers/floats")
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
     matrix_cpy = [row[:] for row in matrix]
