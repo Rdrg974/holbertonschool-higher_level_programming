3f6cfaa0651d621c35fbedae81001f13ca02344a
Abdou Rodrigue Hassany Mohamed <hassany.rodrigue@gmail.com>
2024-02-06 15:52:25 +0000

    Fix the docstring

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,19 +1,14 @@
 #!/usr/bin/python3
-"""Function that adds 2 integers."""
+"""Function that adds 2 integers.
+
+Returns:
+        _type_: the sum of a and b
+"""
 
 
 def add_integer(a, b=98):
-    """Add 2 integers.
-
-    Args:
-        a (int, float): a integer
-        b (int, float): an integer. Defaults to 98.
-
-    Raises:
-        TypeError: a and b must be an integer
-
-    Returns:
-        _type_: the sum of a and b
+    """ Add 2 integers
+    Sum of a and b
     """
     if not (isinstance(a, (int, float)) or isinstance(a, (int, float))):
         raise TypeError("a must be an integer")
