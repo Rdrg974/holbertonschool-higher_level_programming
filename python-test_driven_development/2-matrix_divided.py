c114f5b6e028ff194fc155fb78cd9726c421fde2
Abdou Rodrigue Hassany Mohamed <hassany.rodrigue@gmail.com>
2024-02-07 19:50:44 +0000

    Update

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -24,13 +24,16 @@ def matrix_divided(matrix, div):
         raise ZeroDivisionError("division by zero")
     if not isinstance(matrix, list) or \
             not all(isinstance(row, list) for row in matrix) or \
-            not all(isinstance(num, (int, float)) for num \
-                in [elem for row in matrix for elem in row]):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            not all(isinstance(num, (int, float))
+                    for num in [elem for row in matrix for elem in row]):
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats")
     if len(matrix) == 0:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats")
     if not all(len(row) > 0 for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats")
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
     matrix_cpy = [row[:] for row in matrix]
