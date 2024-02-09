41696428062953f82496e23f327691f6b8fbb271
Abdou Rodrigue Hassany Mohamed <hassany.rodrigue@gmail.com>
2024-02-09 09:41:45 +0000

    Fix pycodestyle

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -46,7 +46,7 @@ class Rectangle:
         if self.width == 0 or self.height == 0:
             return 0
         return (self.width * 2) + (self.height * 2)
-    
+
     def __str__(self):
         """This method returns the string representation of the rectangle."""
         if self.width == 0 or self.height == 0:
