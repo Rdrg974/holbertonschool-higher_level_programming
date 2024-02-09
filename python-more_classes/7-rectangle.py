0d801b4e97f880069ef91f1ca489c42b63969016
Abdou Rodrigue Hassany Mohamed <hassany.rodrigue@gmail.com>
2024-02-09 10:31:26 +0000

    Fix pycostyle

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -55,7 +55,8 @@ class Rectangle:
         """This method returns the string representation of the rectangle."""
         if self.width == 0 or self.height == 0:
             return ""
-        return "\n".join([str(self.print_symbol) * self.width for _ in range(self.height)])
+        return "\n".join([str(self.print_symbol) * self.width
+                          for _ in range(self.height)])
 
     def __repr__(self):
         """This method returns the string representation of the rectangle."""
