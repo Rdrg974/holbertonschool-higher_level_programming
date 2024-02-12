d15e3283786be8f7625a33ae92914b26278eca50
Abdou Rodrigue Hassany Mohamed <hassany.rodrigue@gmail.com>
2024-02-12 14:53:23 +0000

    Fix the super function

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -8,6 +8,7 @@ class Square(Rectangle):
         """Initialize Square"""
         self.integer_validator("size", size)
         self.__size = size
+        super().__init__(self.__size, self.__size)
 
     def area(self):
         """Return the area of the square"""
