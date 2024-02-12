1f3ba52a9bf8c3c4a654d646b5dede20b8c647b6
Abdou Rodrigue Hassany Mohamed <hassany.rodrigue@gmail.com>
2024-02-12 14:19:54 +0000

    Fix pycodestyle

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -2,6 +2,7 @@
 """Class Rectangle"""
 BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     """A class Rectangle that inherits from BaseGeometry"""
     def __init__(self, width, height):
