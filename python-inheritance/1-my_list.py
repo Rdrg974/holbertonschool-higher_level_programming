a3d9259a59da20c72c87d59cb1003b8ba5580162
Abdou Rodrigue Hassany Mohamed <hassany.rodrigue@gmail.com>
2024-02-12 11:01:39 +0000

    Update

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -4,10 +4,6 @@
 
 class MyList(list):
     """Class MyList that inherits from list"""
-    def __init__(self):
-        """Initializes the object"""
-        pass
-
     def print_sorted(self):
         """Print the list sorted"""
         print(sorted(self))
