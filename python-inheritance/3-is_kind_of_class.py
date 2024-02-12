2e42ce4441cc540799d98d88f057439de00e8517
Abdou Rodrigue Hassany Mohamed <hassany.rodrigue@gmail.com>
2024-02-12 12:28:03 +0000

    Check if it's exactly the same

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Check f it's exactly the same"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Returns True if the object is an instance of,
+    or if the object is an instance of a class that inherited from,
+    the specified class ; otherwise False
+    """
+    if type(obj) is a_class or isinstance(obj, a_class):
+        return True
+    return False
