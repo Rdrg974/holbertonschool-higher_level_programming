26f092cf63629ac2eefe6465246ab65a7de72421
Abdou Rodrigue Hassany Mohamed <hassany.rodrigue@gmail.com>
2024-02-12 12:44:34 +0000

    Update

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -3,5 +3,5 @@
 
 
 def inherits_from(obj, a_class):
-    """returns True if the object is an instance of a class that inherited"""
-    return issubclass(type(obj), a_class)
+    """Check if the object is an instance of a class that inherited"""
+    return issubclass(type(obj), a_class) and type(obj) != a_class
