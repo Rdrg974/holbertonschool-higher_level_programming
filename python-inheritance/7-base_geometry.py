686f36e54024798066524669ca175810d121bd0b
Abdou Rodrigue Hassany Mohamed <hassany.rodrigue@gmail.com>
2024-02-12 13:03:46 +0000

    A class BaseGeometry

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""An empty class"""
+
+
+class BaseGeometry:
+    """An empty class"""
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
