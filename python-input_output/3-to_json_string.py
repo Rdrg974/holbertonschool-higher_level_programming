40fc0b7ed2d90b080c5421154f4a0033b62e3362
Abdou Rodrigue Hassany Mohamed <hassany.rodrigue@gmail.com>
2024-02-13 13:15:39 +0000

    Function that returns the JSON representation of an object

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""JSON representation of an object"""
+import json
+
+
+def to_json_string(my_obj):
+    """Return the JSON representation of an object"""
+    return json.dumps(my_obj)
