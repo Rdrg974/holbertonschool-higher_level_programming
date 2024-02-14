2328af765a8706ccd44d27a33f531f4051e413b0
Abdou Rodrigue Hassany Mohamed <hassany.rodrigue@gmail.com>
2024-02-14 13:56:36 +0000

    Function pascal's triangle

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""Pascal's Triangle"""
+
+
+def pascal_triangle(n):
+    """Return a list of lists of integers
+    representing the Pascal’s triangle of n
+    """
+    if n > 0:
+        l_pascal = [[1]]
+        for i in range(n -1):
+            my_list = [1]
+            if i > 0:
+                n = len(l_pascal)
+                for j in range(n - 1):
+                    my_list.append(l_pascal[i][j] + l_pascal[i][j + 1])
+            my_list.append(1)
+            l_pascal.append(my_list)
+        return l_pascal
+    return []
