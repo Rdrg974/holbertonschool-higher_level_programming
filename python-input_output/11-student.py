6c3049c6b4862545e3483050aef43917e169cda4
Abdou Rodrigue Hassany Mohamed <hassany.rodrigue@gmail.com>
2024-02-14 08:32:14 +0000

    A class Student

---
+++ python-input_output/11-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Class Student that defines a student"""
+
+
+class Student:
+    """Class Student"""
+    def __init__(self, first_name, last_name, age):
+        """Instantiation with first_name, last_name and age"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student instance"""
+        if attrs is None:
+            return self.__dict__
+        else:
+            new_dict = {}
+            for key in attrs:
+                if key in self.__dict__:
+                    new_dict[key] = self.__dict__[key]
+            return new_dict
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the Student instance"""
+        for key in json:
+            self.__dict__[key] = json[key]
