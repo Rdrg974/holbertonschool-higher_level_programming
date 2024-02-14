afaf58149a30782a660eb80e7e4ed6a6caec84f4
Abdou Rodrigue Hassany Mohamed <hassany.rodrigue@gmail.com>
2024-02-14 08:22:35 +0000

    A class Student

---
+++ python-input_output/9-student.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Class Student that defines a student"""
+
+
+class Student:
+    """Class Student"""
+    def __init__(self, first_name, last_name, age):
+        """Instantiation with first_name, last_name and age"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """retrieves a dictionary representation of a Student instance"""
+        return self.__dict__
