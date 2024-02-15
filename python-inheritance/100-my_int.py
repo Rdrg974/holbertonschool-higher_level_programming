20b26f49429256a275b7d7c0016b540787e4058c
Abdou Rodrigue Hassany Mohamed <hassany.rodrigue@gmail.com>
2024-02-15 13:41:24 +0000

    A class MyInt that inherits from int

--- python-inheritance/100-my_int.py
+++ python-inheritance/100-my_int.py
@@ -5,9 +5,9 @@
 class MyInt(int):
     """A class MyInt"""
     def __eq__(self, other):
-        """equal equal method"""
-        return super().__eq__(other)
-    
+        """A class MyInt"""
+        return int(self) != int(other)
+
     def __ne__(self, other):
-        """not equal method"""
-        return super().__ne__(other)
+        """A class MyInt"""
+        return int(self) == int(other)
