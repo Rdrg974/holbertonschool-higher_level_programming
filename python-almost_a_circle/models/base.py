7c3fc28c4b591e0a4203ce9a4cda9c48b71a6c60
Abdou Rodrigue Hassany Mohamed <hassany.rodrigue@gmail.com>
2024-02-18 23:28:24 +0000

    Fix doctring

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
+"""A class Base."""
+
 
 class Base:
+    """A clase Base."""
+
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """Initialise.
+
+        Args:
+            id (int): an integer
+        """
         if id is not None:
             self.id = id
         else:
