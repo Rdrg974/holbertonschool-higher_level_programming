bfd9a2b3cf948ac992ded87d280326f3d71d9ef0
Abdou Rodrigue Hassany Mohamed <hassany.rodrigue@gmail.com>
2024-02-19 11:04:08 +0000

    Update a class Square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -16,6 +16,15 @@ class Square(Rectangle):
         """
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+
     def __str__(self):
         """Return a string representation of the square."""
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
