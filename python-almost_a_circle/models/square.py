1ae8ee58c62af1929e392d23d56ceff3edec2798
Abdou Rodrigue Hassany Mohamed <hassany.rodrigue@gmail.com>
2024-02-19 10:08:13 +0000

    A class Square

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""A class Square"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """A class Square that inherits from Rectangle"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initialise.
+
+        Args:
+            size (int): the size of square
+            x (int): an integer. Defaults to 0.
+            y (int): an integer. Defaults to 0.
+            id (int): an integer. Defaults to None.
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Return a str"""
+        return "[Square] ({self.id}) {self.__x}/{self.__y} - {self.__width}"
