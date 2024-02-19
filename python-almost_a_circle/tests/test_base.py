5f407c4c6ec832ff67604c371b2ee0c43f0c5bcd
Abdou Rodrigue Hassany Mohamed <hassany.rodrigue@gmail.com>
2024-02-19 15:05:51 +0100

    Test unittest

---
+++ python-almost_a_circle/tests/test_base.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''Module for Base unit tests.'''
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
+
+
+class TestBase(unittest.TestCase):
+    '''Tests the Base class.'''
+    def test_initialisation(self):
+        b = Base()
+        self.assertEqual(str(type(b)), "<class 'models.base.Base'>")
+        self.assertEqual(b.id, 1)
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
