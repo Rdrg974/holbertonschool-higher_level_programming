66d360a9604da6591a76d89a2d11ae6a54b8dac1
Abdou Rodrigue Hassany Mohamed <hassany.rodrigue@gmail.com>
2024-02-19 15:28:32 +0100

    Fix test unittest

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 '''Module for Base unit tests.'''
 import unittest
+import json
 from models.base import Base
 from models.rectangle import Rectangle
 from models.square import Square
@@ -20,6 +21,34 @@ class TestBase(unittest.TestCase):
         b = Base()
 
         self.assertEqual(b.id, a.id + 1)
+
+    def test_saving_id(self):
+        """Test Base(89)"""
+        b = Base(89)
+
+        self.assertEqual(b.id, 89)
+
+    def test_to_json_string_none(self):
+        """Args is None"""
+        json_dictionary = Base.to_json_string(None)
+
+        self.assertTrue(json_dictionary, "null")
+
+    def test_to_json_string_empty_list(self):
+        """Empty list"""
+        json_dictionary = Base.to_json_string([])
+
+        self.assertTrue(json_dictionary, [])
+
+    def test_to_json_string_id(self):
+        """List contains [ { 'id': 12 }]"""
+        input_data = [ { 'id': 12 }]
+        expected_output = json.dumps(input_data)
+        json_dictionary = Base.to_json_string(input_data)
+
+        self.assertTrue(json_dictionary, [ { 'id': 12 }])
+        self.assertEqual(json_dictionary, expected_output)
+
 
 if __name__ == "__main__":
     unittest.main()
