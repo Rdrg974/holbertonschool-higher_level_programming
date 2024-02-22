a9a11b6ec686012c822fe27c048c4d2990972c26
Abdou Rodrigue Hassany Mohamed <hassany.rodrigue@gmail.com>
2024-02-22 17:01:54 +0100

    Added the function draw that opens a window and draws all the Rectangles and Squares

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """A class Base."""
+import random
 import turtle
 import json
 import os
@@ -64,16 +65,22 @@ class Base:
             list_dict = cls.from_json_string(json_str)
             return [cls.create(**d) for d in list_dict]
 
+    def random_color():
+        r = random.random()
+        g = random.random()
+        b = random.random()
+        return (r, g, b)
+    
     @staticmethod
     def draw(list_rectangles, list_squares):
         """Draw rectangles and squares."""
         turtle.title("Draw")
         turtle.bgcolor("black")
         t = turtle.Turtle()
-        t.color("yellow", "red")
         t.shape("turtle")
         t.speed(1)
         for r in list_rectangles:
+            t.color(Base.random_color())
             t.penup()
             t.goto(r.x, r.y)
             t.pendown()
@@ -86,6 +93,7 @@ class Base:
             t.forward(r.height)
             t.left(90)
         for s in list_squares:
+            t.color(Base.random_color())
             t.penup()
             t.goto(s.x, s.y)
             t.pendown()
@@ -97,3 +105,82 @@ class Base:
             t.left(90)
             t.forward(s.size)
             t.left(90)
+            
+    @staticmethod
+    def draw_name(letter):
+        """Draw my name"""
+        screen = turtle.Screen()
+        screen.title("MY NAME")
+        screen.bgcolor("Black")
+        t = turtle.Turtle()
+        t.shape("turtle")
+        t.speed(2)
+        t.width(3)
+
+        positions = {
+        'R': [(-200, 100), (-150, 100), (-150, 50), (-200, 50), (-150, 0)],
+        'O': [(-125, 0), (-125, 100), (-75, 100), (-75, 0), (-125, 0)],
+        'D': [(-50, 0), (-50, 100), (-35, 100), (0, 75), (0, 25), (-35, 0), (-50, 0)],
+        'R2': [(25, 0), (25, 100), (75, 100), (75, 50), (25, 50), (75, 0)],
+        'I': [(100, 0), (100, 100), (100, 0)],
+        'G': [(125, 0), (125, 100), (175, 100), (125, 100), (125, 0), (175, 0), (175, 50), (165, 50), (175, 50), (175, 0)],
+        'U': [(200, 0), (200, 100), (200, 0), (250, 0), (250, 100), (250, 0)],
+        'E': [(275, 0), (275, 100), (325, 100), (275, 100), (275, 50), (325, 50), (275, 50), (275, 0), (325, 0)]}
+        
+        me = {
+        'I': [(-200, -50), (-200, -150)]}
+        am = {
+        'A': [(-150, -150), (-150, -50), (-100, -50), (-100, -100), (-150, -100), (-100, -100), (-100, -150)],
+        'M': [(-75, -150), (-75, -50), (-50, -100), (-25, -50), (-25, -150)]}
+        sexy = {
+        'S': [(25, -50), (25, -100), (75, -100), (75, -150), (25, -150)],
+        'E': [(100, -150), (100, -50), (150, -50), (100, -50), (100, -100), (150, -100), (100, -100), (100, -150)],
+        'X': [(175, -150), (200, -100), (175, -50), (200, -100), (225, -50), (200, -100), (225, -150)],
+        'Y': [(275, -150), (275, -100), (250, -50), (275, -100), (300, -50)]}
+        
+        coeur = [(-200, -250), (-200, -225), (-191.66, -200), (-183.33, -200), (-175, -225), (-166.66, -200), (-158.33, -200), (-150, -225), (-150, -250), (-175, -300)]
+    
+        t.penup()
+        t.goto(-200, 0)
+        t.pendown()
+        for pos in positions.values():
+            for i in pos:
+                t.color(Base.random_color(), Base.random_color())
+                t.goto(i)
+
+        t.penup()
+        t.goto(-200, -150)
+        t.pendown()
+        for pos in me.values():
+            for i in pos:
+                t.color(Base.random_color(), Base.random_color())
+                t.goto(i)
+
+        
+        t.penup()
+        t.goto(-150, -150)
+        t.pendown()
+        for pos in am.values():
+            for i in pos:
+                t.color(Base.random_color(), Base.random_color())
+                t.goto(i)
+                
+        t.penup()
+        t.goto(75, -50)
+        t.pendown()
+        for pos in sexy.values():
+            for i in pos:
+                t.color(Base.random_color(), Base.random_color())
+                t.goto(i)
+        
+        t.penup()
+        t.goto(-175, -300)
+        t.pendown()
+        for pos in coeur:
+            t.color(Base.random_color(), Base.random_color())
+            t.goto(pos)
+        
+        t.penup()
+        t.goto(-175, -250)
+        t.left(90)
+        screen.exitonclick()
