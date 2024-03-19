b069dca91708e16cd0cab78b2c831f8a15f819ab
Abdou Rodrigue Hassany Mohamed <hassany.rodrigue@gmail.com>
2024-03-19 17:21:59 +0100

    Update

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,9 +1,9 @@
 -- Cities table
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities
 (
     `id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     `state_id` INT NOT NULL,
     `name` VARCHAR(256) NOT NULL,
-    FOREIGN KEY (`state_id`) REFERENCES `states`(`id`)
-)
+    FOREIGN KEY (state_id) REFERENCES states(id)
+);
