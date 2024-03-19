67796b9d5a794e3f100685b29aa2f1e0e4261c74
Abdou Rodrigue Hassany Mohamed <hassany.rodrigue@gmail.com>
2024-03-19 17:13:02 +0100

    Update

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,10 +1,9 @@
 -- Cities table
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`
-USE `hbtn_0d_usa`
-CREATE TABLE IF NOT EXISTS `cities`
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities`
 (
-    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     `state_id` INT NOT NULL,
     `name` VARCHAR(256) NOT NULL,
-    FOREIGN KEY (state_id) REFERENCES states(id)
+    FOREIGN KEY (`state_id`) REFERENCES `states`(`id`)
 )
--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,6 @@
 -- Cities by States
 SELECT `cities.id`, `cities.name`, `states.name`
-FROM `cities` JOIN `states` ON `cities.state_id` = `states.id`
+FROM `cities`
+JOIN `states`
+    ON `cities.state_id` = `states.id`
 ORDER BY `cities.id` ASC;
