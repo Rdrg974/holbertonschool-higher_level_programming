ef1a36db5514c6a4cdce03da0806f7f554996d1d
Abdou Rodrigue Hassany Mohamed <hassany.rodrigue@gmail.com>
2024-03-19 17:17:15 +0100

    Update

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- Cities by States
 SELECT `cities.id`, `cities.name`, `states.name`
 FROM `cities`
-JOIN `states`
+INNER JOIN `states`
     ON `cities.state_id` = `states.id`
 ORDER BY `cities.id` ASC;
