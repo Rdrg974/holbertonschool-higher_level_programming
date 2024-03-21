5d984737fae2350320464f9eafaaf35622692e4f
Abdou Rodrigue Hassany Mohamed <hassany.rodrigue@gmail.com>
2024-03-21 10:12:34 +0100

    Update code

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
                          passwd=argv[2], db=argv[3])
 
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM `states` WHERE `name` LIKE 'N%' ORDER BY `id` ASC")
+    cursor.execute("SELECT * FROM `states` WHERE `name` LIKE 'N%' ORDER BY states.id ASC")
     rows = cursor.fetchall()
 
     for row in rows:
