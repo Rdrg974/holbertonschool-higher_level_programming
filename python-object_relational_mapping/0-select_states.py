bda9e04fade063298bba2f5d88f0fc804e3a5c74
Abdou Rodrigue Hassany Mohamed <hassany.rodrigue@gmail.com>
2024-03-21 10:09:10 +0100

    Fix typo

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
                          passwd=argv[2], db=argv[3])
 
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states")
+    cursor.execute("SELECT * FROM `states`")
     rows = cursor.fetchall()
 
     for row in rows:
--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
                          passwd=argv[2], db=argv[3])
 
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+    cursor.execute("SELECT * FROM `states` WHERE `name` LIKE 'N%'")
     rows = cursor.fetchall()
 
     for row in rows:
