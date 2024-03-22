ddf4245d75713d0353a39c1d8739b01fff5a1e7d
Abdou Rodrigue Hassany Mohamed <hassany.rodrigue@gmail.com>
2024-03-22 13:36:49 +0100

    Fix pycodestyle

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     Access to the database and get the cities
     """
     db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
-                            passwd=argv[2], db=argv[3])
+                         passwd=argv[2], db=argv[3])
 
     with db.cursor() as curseur:
         curseur.execute("SELECT cities.id, cities.name\
