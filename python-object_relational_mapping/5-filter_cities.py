beaf77659b2f06a7b7998b449b98d4f25acedb08
Abdou Rodrigue Hassany Mohamed <hassany.rodrigue@gmail.com>
2024-03-22 13:02:29 +0100

    Update

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -14,13 +14,13 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
                             passwd=argv[2], db=argv[3])
 
-    with db.cursor() as cur:
-        cur.execute("SELECT cities.id, cities.name\
+    with db.cursor() as curseur:
+        curseur.execute("SELECT cities.id, cities.name\
                     FROM cities\
                     JOIN states\
                     ON cities.state_id = states.id\
                     WHERE states.name LIKE BINARY %(state_name)s\
                     ORDER BY cities.id ASC", {'state_name': argv[4]})
-        rows = cur.fetchall()
+        rows = curseur.fetchall()
     if rows is not None:
         print(", ".join([row[1] for row in rows]))
