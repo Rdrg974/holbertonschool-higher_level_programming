634ee0f7186af17a124b8a5914c1428e1558e69b
Abdou Rodrigue Hassany Mohamed <hassany.rodrigue@gmail.com>
2024-05-20 06:47:33 +0200

    Toggle classes

---
+++ javascript-dom_manipulation/3-script.js
@@ -0,0 +1,4 @@
+document.getElementById('toggle_header').addEventListener('click', function () {
+  document.querySelector('header').classList.toggle('green');
+  document.querySelector('header').classList.toggle('red');
+});
